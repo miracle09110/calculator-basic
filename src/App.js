15a60cbc51289567cf71433c8c58d754353e0fd8
Justin <earon.pabon@gmail.com>
2020-06-15 21:38:06 +0800

    Adding operation handler

--- src/App.js
+++ src/App.js
@@ -4,36 +4,78 @@ import './App.css';
 class App extends Component {
 
   state = {
-    result: 0
+    result: 0,
+    currentInput: 0,
+    previousInput: 0,
+    currentOperation: null
   }
 
-  addButton = () =>{
+  operationClickHandler = (operation) =>{
+    let firstInput = this.state.currentInput;
+    let result = 0;
+    if (this.state.currentOperation) {
+      result = this.performOperation()
+      firstInput = result;
+    }    
+
+
+    this.setState({ 
+      currentOperation: operation,
+      previousInput: firstInput,
+      currentInput: 0,
+      result: result
+    });
+  }
+
+  performOperation = () =>{
+    switch (this.state.currentOperation) {
+      case '+':
+        return this.state.currentInput + this.state.previousInput;
+      case '-':
+        return this.state.previousInput- this.state.currentInput;
+      case 'x':
+        return this.state.currentInput * this.state.previousInput;
+      case '/':
+        return this.state.previousInput / this.state.currentInput;
+    }
+  }
+  
+
+  digitClickedHandler = (number) =>{
+    let input = number;
+    if ( this.state.currentInput !== 0 ){
+      input = parseInt(`${this.state.currentInput}${input}`);
+    }
+
     this.setState({
-      result : this.state.result + 1
-    })
+      currentInput: input,
+    });
   }
 
   render(){
     return (
       <div className="App">
-        <div>{this.state.result}</div>
+        <div>Result: {this.state.result}</div>
+        <div>First Input: {this.state.currentInput}</div>
+        <div>Operation: {this.state.currentOperation}</div>
+        <div>Second Input: {this.state.previousInput}</div>
         <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
+          <button onClick={() => this.digitClickedHandler(1)}>1</button>
+          <button onClick={() => this.digitClickedHandler(2)}>2</button>
+          <button onClick={() => this.digitClickedHandler(3)}>3</button>
+          <button onClick={() => this.digitClickedHandler(4)}>4</button>
+          <button onClick={() => this.digitClickedHandler(5)}>5</button>
+          <button onClick={() => this.digitClickedHandler(6)}>6</button>
+          <button onClick={() => this.digitClickedHandler(7)}>7</button>
+          <button onClick={() => this.digitClickedHandler(8)}>8</button>
+          <button onClick={() => this.digitClickedHandler(9)}>9</button>
+          <button onClick={() => this.digitClickedHandler(0)}>0</button>
         </div>
         <div>
-          <button onClick={this.addButton}>+</button>
-          <button>-</button>
-          <button>x</button>
-          <button>/</button>
+          <button onClick={() => this.operationClickHandler('+')}>+</button>
+          <button onClick={() => this.operationClickHandler('-')}>-</button>
+          <button onClick={() => this.operationClickHandler('x')}>x</button>
+          <button onClick={() => this.operationClickHandler('/')}>/</button>
         </div>
       </div>
     );
