80ddbbf87173d7bd580870a81d1aab2867cafa77
Justin <earon.pabon@gmail.com>
2020-06-15 20:40:45 +0800

    Adding only buttons

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    result: 0
+  }
+
   render(){
     return (
       <div className="App">
-      
+        <div>{this.state.result}</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>0</button>
+        </div>
+        <div>
+          <button>+</button>
+          <button>-</button>
+          <button>x</button>
+          <button>/</button>
+        </div>
       </div>
     );
   }
